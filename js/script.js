95b6338153cabeff8e1aed4b2110dea1308f13c6
rohsikdnan <90635399+rohsikdnan@users.noreply.github.com>
2022-12-31 18:56:52 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,10 +4,11 @@ let selected_cell = -1;
 
 
 cells.forEach((cell, index) => {
-    if (index % 27 < 9) cell.style.borderTop = '0.185rem solid black';
-    if (index % 9 == 0 || index % 9 == 3 || index % 9 == 6) cell.style.borderLeft = '0.185rem solid black';
-    if (index % 9 == 8) cell.style.borderRight = '0.185rem solid black';
-    if (index >= 72 && index <= 80) cell.style.borderBottom = '0.185rem solid black';
+    let width = "0.185rem"
+    if (index % 27 < 9) cell.style.borderTopWidth = width;
+    if (index % 9 == 0 || index % 9 == 3 || index % 9 == 6) cell.style.borderLeftWidth =width;
+    if (index % 9 == 8) cell.style.borderRightWidth = width;
+    if (index >= 72 && index <= 80) cell.style.borderBottomWidth =width;
 })
 
 
@@ -30,8 +31,43 @@ const printSudoku = () => {
     }
 }
 
+const heighlightRegion = () => {
+    let row = Math.floor(selected_cell / size);
+    let col = selected_cell % size;
+
+    cells.forEach(e => e.classList.remove("sameRegion"));
+
+
+    for (let i = 0; i < 9; i++) {
+        cells[9 * i + col].classList.add("sameRegion")
+        cells[9 * row + i].classList.add("sameRegion")
+    }
+
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            cells[9 * (row - row % 3 + i) + (col - col % 3 + j)].classList.add("sameRegion")
+        }
+    }
+}
+
+const heighlightNumber = (num) => {
+    cells.forEach(e => e.classList.remove("sameNumber"));
+    if (num) {
+        for (let i = 0; i < size; i++) {
+            for (let j = 0; j < size; j++) {
+                let index = 9 * i + j;
+                if (cells[index].innerHTML == num) {
+                    cells[index].classList.add("sameNumber")
+                }
+
+            }
+        }
+    }
+}
+
 const checkErr = (value) => {
-    cells.forEach(e => e.classList.remove("err"))
+    cells.forEach(e => e.classList.remove("err"));
+
     let row = Math.floor(selected_cell / size);
     let col = selected_cell % size;
 
@@ -39,15 +75,11 @@ const checkErr = (value) => {
         if (cell.innerHTML == value && !cell.classList.contains("selected")) {
             cell.classList.add("err");
             // cells[selected_cell].classList.add("wrong");
-
         };
     }
 
     for (let i = 0; i < 9; i++) {
         addErr(cells[9 * i + col])
-    }
-
-    for (let i = 0; i < 9; i++) {
         addErr(cells[9 * row + i])
     }
 
@@ -66,7 +98,9 @@ cells.forEach((cell, i) => {
         cells.forEach(e => e.classList.remove('selected'));
         selected_cell = i;
         console.log(i)
-        cell.classList.add('selected')
+        cell.classList.add('selected');
+        heighlightRegion();
+        heighlightNumber(cell.innerHTML)
     });
 });
 
@@ -80,4 +114,4 @@ numbers.forEach((num, index) => {
     })
 });
 
-// printSudoku()
+printSudoku()
